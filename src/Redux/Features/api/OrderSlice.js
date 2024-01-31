b0d7e2a30c756fffde5ffb0e69cdd93373158681
ifti2979 <salminifti79@gmail.com>
2024-02-01 02:00:12 +0600

    query Updated

---
+++ src/Redux/Features/api/OrderSlice.js
@@ -0,0 +1,22 @@
+// import { apiSlice } from "./apiSlice";
+
+// const USERS_URL = "/api/users";
+
+// export const orderApiSlice = apiSlice.injectEndpoints({
+//   endpoints: (builder) => ({
+//     getAllOrder: builder.query({
+//         query: (id) => `/orderList/${id}`,
+//         keepUnusedDataFor: 600,
+//         providesTags: ['SingleOrderAndDetails']
+//     }),
+//   }),
+//   }),
+// });
+// export const {
+//   useLoginMutation,
+//   useLogoutMutation,
+//   useCreateUserMutation,
+//   useUpdateMutation,
+//   useGetallusersQuery,
+//   useGetUserByIdQuery,
+// } = orderApiSlice;
