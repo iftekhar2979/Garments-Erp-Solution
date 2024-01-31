1a8b431e5cd409d237b72e4ad845965dfe22f0ae
Iftekhar <97031620+iftekhar2979@users.noreply.github.com>
2024-02-01 02:02:42 +0600

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
  //Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyDaa3JB3VeDRgvNFLjMfPERnQBvv3eImmw",
-  authDomain: "garments-erp.firebaseapp.com",
-  projectId: "garments-erp",
-  storageBucket: "garments-erp.appspot.com",
-  messagingSenderId: "298443362909",
-  appId: "1:298443362909:web:9f98b2cc5bba3b5cc6b3b4"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
